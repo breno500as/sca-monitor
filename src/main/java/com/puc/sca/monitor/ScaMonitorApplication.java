595e2b2ec244049db90d6d7eb8c6077c542c458e
breno500as <breno500as@gmail.com>
2019-07-23 22:06:51 -0300

    sca-monitor

---
+++ src/main/java/com/puc/sca/monitor/ScaMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.puc.sca.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puc/sca/monitor/ScaMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.puc.sca.monitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScaMonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
