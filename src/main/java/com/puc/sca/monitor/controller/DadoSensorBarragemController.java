fc562752b48a322a30adbdfc22ad5a3dcacce597
breno.mcosta <breno.mcosta@pbh.gov.br>
2019-07-26 16:48:51 -0300

    get dados

--- src/main/java/com/puc/sca/monitor/controller/DadoSensorBarragemController.java
+++ src/main/java/com/puc/sca/monitor/controller/DadoSensorBarragemController.java
@@ -1,6 +1,7 @@
 package com.puc.sca.monitor.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +15,17 @@ import com.puc.sca.monitor.repository.DadoSensorBarragemRepository;
 public class DadoSensorBarragemController {
 
 	@Autowired
-	private DadoSensorBarragemRepository dadoSensorBarragem;
+	private DadoSensorBarragemRepository dadoSensorBarragemRepository;
 
 	@PostMapping
 	public String incluiDadosSensor(@RequestBody DadoSensorBarragem dadoSensorBarragem) {
-		this.dadoSensorBarragem.save(dadoSensorBarragem);
+		this.dadoSensorBarragemRepository.save(dadoSensorBarragem);
 		return "Ok";
+	}
+	
+	@GetMapping
+	public Iterable<DadoSensorBarragem> getDadosSensor() {
+		return this.dadoSensorBarragemRepository.findAll();
 	}
 
 }
