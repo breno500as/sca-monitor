443234580bfb05bce6b14eecaca5427ef9c8b576
breno500as <breno500as@gmail.com>
2019-07-27 23:40:12 -0300

    configurações jms

--- src/main/java/com/puc/sca/monitor/controller/DadoSensorBarragemController.java
+++ src/main/java/com/puc/sca/monitor/controller/DadoSensorBarragemController.java
@@ -1,12 +1,14 @@
 package com.puc.sca.monitor.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.puc.sca.integration.pojo.Alerta;
 import com.puc.sca.monitor.model.DadoSensorBarragem;
 import com.puc.sca.monitor.repository.DadoSensorBarragemRepository;
 
@@ -16,10 +18,19 @@ public class DadoSensorBarragemController {
 
 	@Autowired
 	private DadoSensorBarragemRepository dadoSensorBarragemRepository;
+	
+	@Autowired 
+	private JmsTemplate jmsTemplateTopic;
 
 	@PostMapping
 	public String incluiDadosSensor(@RequestBody DadoSensorBarragem dadoSensorBarragem) {
+		
+
+		this.jmsTemplateTopic.convertAndSend("topic.alerta-box",new Alerta(dadoSensorBarragem.getNivelAlerta().toString()));
+		
+		
 		this.dadoSensorBarragemRepository.save(dadoSensorBarragem);
+		
 		return "Ok";
 	}
 	
