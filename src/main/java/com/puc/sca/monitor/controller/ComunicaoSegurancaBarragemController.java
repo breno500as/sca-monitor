5b9fb9cdf7c654e99577d18ff1366288e1ff2305
breno.mcosta <breno.mcosta@pbh.gov.br>
2019-08-09 16:57:52 -0300

    timeout

--- src/main/java/com/puc/sca/monitor/controller/ComunicaoSegurancaBarragemController.java
+++ src/main/java/com/puc/sca/monitor/controller/ComunicaoSegurancaBarragemController.java
@@ -1,5 +1,7 @@
 package com.puc.sca.monitor.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.puc.sca.integration.util.Alerta;
+import com.puc.sca.integration.util.Constants;
 import com.puc.sca.monitor.service.ModuloAlertaService;
 
 @RestController
@@ -18,11 +21,14 @@ public class ComunicaoSegurancaBarragemController {
 
 
 	@PostMapping
-	public String informaSistemasIntegrados(@RequestBody Alerta alerta) {
+	public void informaSistemasIntegrados(@RequestBody Alerta alerta, HttpServletRequest request) {
+		
+		alerta.setNomeUsuarioLogado(request.getParameter(Constants.NOME_USUARIO_LOGADO));
+		alerta.setEmailUsuarioLogado(request.getParameter(Constants.EMAIL_USUARIO_LOGADO));
+		
 		this.moduloAlertaService.acionaModuloSegurancaComunicacaoEvacuacao(alerta);
 		this.moduloAlertaService.acionaModuloSegurancaComunicacaoSitemaCorpoDeBombeiros(alerta);
 		this.moduloAlertaService.acionaModuloSegurancaIntegracaoSistemaDefesaCivil(alerta);
-		return "ok";
 	}
 
  
