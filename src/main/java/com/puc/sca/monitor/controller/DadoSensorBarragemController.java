145c22b8ae448101753751a190680a58c6267e30
breno <breno@breno-Inspiron-7580>
2019-08-23 11:10:48 -0300

    Paginação

--- src/main/java/com/puc/sca/monitor/controller/DadoSensorBarragemController.java
+++ src/main/java/com/puc/sca/monitor/controller/DadoSensorBarragemController.java
@@ -1,10 +1,14 @@
 package com.puc.sca.monitor.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.puc.sca.integration.util.Alerta;
@@ -41,8 +45,11 @@ public class DadoSensorBarragemController {
 	}
 	
 	@GetMapping
-	public Iterable<DadoSensorBarragem> getDadosSensor() {
-		return this.dadoSensorBarragemRepository.findAll();
+	public Iterable<DadoSensorBarragem> getDadosSensor(@RequestParam("page") Integer page, 
+            @RequestParam("size") Integer size,
+            @RequestParam(name = "sort", defaultValue = "id") String sort) {
+		Pageable pageable = PageRequest.of(page -1, size, Sort.by(sort));
+		return this.dadoSensorBarragemRepository.findAll(pageable);
 	}
 
 }
--- src/main/java/com/puc/sca/monitor/repository/DadoSensorBarragemRepository.java
+++ src/main/java/com/puc/sca/monitor/repository/DadoSensorBarragemRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.puc.sca.monitor.model.DadoSensorBarragem;
 
-@RepositoryRestResource(path = "dados-sensor", collectionResourceRel = "dados-sensor")
 public interface DadoSensorBarragemRepository extends PagingAndSortingRepository<DadoSensorBarragem, String> {
 
 }
